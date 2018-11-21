e8d5effd65280ed6aadfec45b9de5310dcbbb7c6
Kristaps Grinbergs <fassko@gmail.com>
2018-11-21 18:48:36 +0200

    Initial Commit

---
+++ Native Swift Editor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Native Swift Editor
+//
+//  Created by Kristaps Grinbergs on 21/11/2018.
+//  Copyright © 2018 fassko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
